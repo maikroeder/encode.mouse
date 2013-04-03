df124cd912850e13691da72c335c1f140d0c7c2c
maikroeder <maikroeder@gmail.com>
2013-04-03 14:34:44 +0200

    =add

---
+++ encode/mouse/page/__init__.py
@@ -0,0 +1,14 @@
+import codecs
+from mrbob.rendering import parse_variables
+from pkg_resources import get_provider
+
+def rendered_var(variables, rendered):
+    for render in rendered:
+        provider = get_provider('encode.mouse')
+        file_name = provider.get_resource_filename('encode.mouse', '%s/output/%s' % (render, render))
+        variables[render] = codecs.open(file_name, 'r', 'utf-8').read()
+
+def pre_render(self):
+    self.variables = parse_variables(self.variables)
+    rendered_var(self.variables, ['overlay', 'footer'])
+    
